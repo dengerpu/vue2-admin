96573d004323e359e76253d0051ed9fe945aed30
dep <1649743146@qq.com>
2023-04-05 22:19:41 +0800

    feat: 封装axios

---
+++ src/api/login.js
@@ -0,0 +1,22 @@
+import request from 'src/utils/request'
+
+/***
+ * 登陆请求
+ * @param username 用户名
+ * @param password 密码
+ * @param code 验证码
+ * @param uuid
+ * @returns {*}
+ */
+export function login (username, password, code, uuid) {
+  return request({
+    url: '/login',
+    method: 'post',
+    data: {
+      username,
+      password,
+      code,
+      uuid
+    }
+  })
+}
---
+++ src/utils/request.js
@@ -0,0 +1,52 @@
+import axios from 'axios'
+import { Message } from 'element-ui'
+import { store } from 'src/store/index'
+import { getToken } from 'src/utils/auth'
+
+// 创建axios实例
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API, // url = baseURL + requestURL
+  timeout: 5000
+})
+
+// 请求拦截
+service.interceptors.request.use(
+  config => {
+    // TODO 这里是用store.getters.token 还是 getToken()
+    if (store.getters.token) {
+      // 每次请求将token携带过去
+      config.headers.Authorization = getToken()
+    }
+  },
+  error => {
+    console.log(error)
+    return Promise.reject(error)
+  }
+)
+
+// 响应拦截
+service.interceptors.response.use(
+  response => {
+    const res = response.data
+
+    if (res.code !== 200) {
+      Message({
+        message: res.message || 'Error',
+        type: 'error',
+        duration: 5 * 1000
+      })
+    } else {
+      return res
+    }
+  },
+  error => {
+    Message({
+      message: error.message || 'Error',
+      type: 'error',
+      duration: 5 * 1000
+    })
+    return Promise.reject(error)
+  }
+)
+
+export default service
