322d020e9c81d35667de8fbf2500f5fb160bfad1
dep <1649743146@qq.com>
2023-04-20 15:09:04 +0800

    refactor: 更改element-ui为全局引入

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './plugins/element.js'
 import './styles/index.scss'
-
 import './icons' // 引入icons(全局注册SvgIcon和svg)
+
+import ElementUI from 'element-ui'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
