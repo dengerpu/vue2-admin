1c321b0c1b478605ad235fa9423d37e11fd3d18b
dep <1649743146@qq.com>
2023-05-04 11:51:04 +0800

    feat: 新增UserActivity文章组件

---
+++ src/views/profile/components/DUserActivity.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="d-user-activity-container">
+    <div class="d-user-activity-header">
+      <div class="d-user-activity-user-icon">
+        <img :src="authorImg" alt="">
+      </div>
+      <div class="d-user-activity-info">
+        <div class="d-user-activity-info-author">{{ authorName }}</div>
+        <div class="d-user-activity-info-notes">
+          <span class="d-user-activity-info-notes-from">{{ articleFrom }}</span>
+          <span class="divider">-</span>
+          <span class="d-user-activity-info-notes-time">{{ date }}</span>
+        </div>
+      </div>
+    </div>
+    <div class="d-user-activity-content">
+      <slot/>
+    </div>
+    <div class="d-user-activity-footer">
+      <ul class="d-user-activity-footer-info">
+        <li>
+          <span class="d-user-activity-footer-link">
+            <i class="el-icon-map-location"></i>
+            ChongQing
+          </span>
+        </li>
+        <li>
+          <span class="d-user-activity-footer-link">
+            <i class="el-icon-share"></i>
+            share
+          </span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DUserActivity',
+  components: {},
+  props: {
+    authorImg: {
+      type: String,
+      required: true
+    },
+    authorName: {
+      type: String,
+      require: true
+    },
+    articleFrom: {
+      type: String,
+      default: '@爱写bug的小邓程序员'
+    },
+    date: {
+      type: String
+    }
+  },
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.d-user-activity-container {
+  color: #666;
+  margin-bottom: 20px;
+  padding-bottom: 15px;
+  border-bottom: 1px solid #d2d6de;
+  .d-user-activity-header {
+    display: flex;
+    .d-user-activity-user-icon {
+      width: 40px;
+      height: 40px;
+      border-radius: 50%;
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+      }
+    }
+    .d-user-activity-info-author {
+      color: #000;
+    }
+    .d-user-activity-info {
+      margin-left: 10px;
+      .d-user-activity-info-notes {
+        padding: 2px 0;
+        font-weight: 500;
+        font-size: 12px;
+      }
+    }
+  }
+  .d-user-activity-content {
+    padding: 10px 0;
+    font-size: 14px;
+  }
+  .d-user-activity-footer {
+    font-size: 14px;
+    .d-user-activity-footer-info {
+      padding: 5px 0;
+      margin-bottom: 10px;
+      display: flex;
+      li:nth-child(even){
+        padding: 0 10px;
+      }
+    }
+  }
+}
+.divider {
+  padding: 0 2px;
+}
+</style>
--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -6,7 +6,17 @@
     <el-col :span="18">
       <el-card>
         <el-tabs v-model="activeTab">
-          <el-tab-pane label="Activity" name="activity">activity</el-tab-pane>
+          <el-tab-pane label="Activity" name="activity">
+            <d-user-activity author-img="https://wpimg.wallstcn.com/57ed425a-c71e-4201-9428-68760c0537c4.jpg?imageView2/1/w/80/h/80" author-name="Iron Man" article-from="Shared publicly" date="7:30 PM today">
+              <p>Lorem ipsum represents a long-held tradition for designers, typographers and the like. Some people hate it and argue for its demise, but others ignore the hate as they create awesome tools to help create filler text for everyone from bacon lovers to Charlie Sheen fans.</p>
+            </d-user-activity>
+            <d-user-activity author-img="https://wpimg.wallstcn.com/9e2a5d0a-bd5b-457f-ac8e-86554616c87b.jpg?imageView2/1/w/80/h/80" author-name="Captain American" date="7:30 PM today">
+              Lorem ipsum represents a long-held tradition for designers, typographers and the like. Some people hate it and argue for its demise, but others ignore the hate as they create awesome tools to help create filler text for everyone from bacon lovers to Charlie Sheen fans.
+            </d-user-activity>
+            <d-user-activity author-img="https://wpimg.wallstcn.com/fb57f689-e1ab-443c-af12-8d4066e202e2.jpg?imageView2/1/w/80/h/80" author-name="Captain American" date="7:30 PM today">
+              Lorem ipsum represents a long-held tradition for designers, typographers and the like. Some people hate it and argue for its demise, but others ignore the hate as they create awesome tools to help create filler text for everyone from bacon lovers to Charlie Sheen fans.
+            </d-user-activity>
+          </el-tab-pane>
           <el-tab-pane label="Timeline" name="timeline">timeline</el-tab-pane>
           <el-tab-pane label="Account" name="account">account</el-tab-pane>
         </el-tabs>
@@ -17,10 +27,11 @@
 
 <script>
 import DUserCard from '@/views/profile/components/DUserCard'
+import DUserActivity from '@/views/profile/components/DUserActivity'
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: 'Profile',
-  components: { DUserCard },
+  components: { DUserActivity, DUserCard },
   props: {},
   data () {
     return {
