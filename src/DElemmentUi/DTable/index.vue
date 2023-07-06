00ef1cdfd930960c6fb9e3311f0733abf170763e
dep <1649743146@qq.com>
2023-07-06 22:38:29 +0800

    feat: 新增底部分页

--- src/DElemmentUi/DTable/index.vue
+++ src/DElemmentUi/DTable/index.vue
@@ -1,25 +1,73 @@
 <template>
   <div class="d-table-container">
+    <div class="d-table-query-container">
+      <slot name="query"></slot>
+    </div>
     <div class="d-table-options-container">
-      <el-button type="primary" :size="size" icon="el-icon-plus"
-        >新增</el-button
-      >
-      <el-button type="success" :size="size" icon="el-icon-edit"
-        >修改</el-button
-      >
-      <el-button type="danger" :size="size" icon="el-icon-delete"
-        >删除</el-button
-      >
-      <el-button type="warning" :size="size" icon="el-icon-download"
-        >导出</el-button
-      >
+      <el-form>
+        <el-form-item>
+          <slot name="add"></slot>
+          <el-button
+            v-if="!$slots.add"
+            type="primary"
+            :size="size"
+            icon="el-icon-plus"
+            >新增</el-button
+          >
+        </el-form-item>
+        <el-form-item>
+          <slot name="see"></slot>
+          <el-button
+            v-if="!$slots.see"
+            type="success"
+            :style="{
+              backgroundColor: '#13ce66',
+              borderColor: '#13ce66'
+            }"
+            :size="size"
+            icon="el-icon-view"
+            >查看</el-button
+          >
+        </el-form-item>
+        <el-form-item>
+          <slot name="edit"></slot>
+          <el-button
+            v-if="!$slots.edit"
+            type="success"
+            :size="size"
+            icon="el-icon-edit"
+            >修改</el-button
+          >
+        </el-form-item>
+        <el-form-item>
+          <slot name="del"></slot>
+          <el-button
+            v-if="!$slots.del"
+            type="danger"
+            :size="size"
+            icon="el-icon-delete"
+            >删除</el-button
+          >
+        </el-form-item>
+        <el-form-item>
+          <slot name="export"></slot>
+          <el-button
+            v-if="!$slots.export"
+            type="warning"
+            :size="size"
+            icon="el-icon-download"
+            >导出</el-button
+          >
+        </el-form-item>
+      </el-form>
+      <slot name="button"></slot>
     </div>
     <el-table
       :data="defaultTableData"
       :stripe="stripe"
       :highlight-current-row="isRadio"
       :size="size"
-      @current-change="handleCurrentChange"
+      @current-change="handleTableCurrentChange"
     >
       <el-table-column type="index" width="50" label="序号"> </el-table-column>
       <template v-if="dataProps && dataProps.length > 0">
@@ -34,7 +82,7 @@
           </template>
         </el-table-column>
       </template>
-      <slot></slot>
+      <slot name="table"></slot>
       <el-table-column fixed="right" label="操作" width="100">
         <template v-slot="scope">
           <el-button @click="handleClick(scope.row)" type="text" size="small"
@@ -44,13 +92,32 @@
         </template>
       </el-table-column>
     </el-table>
+    <div class="d-table-pagination-container">
+      <div
+        class="d-table-pagination"
+        :class="['d-table-pagination-' + pagePosition]"
+      >
+        <slot name="pagination"></slot>
+        <el-pagination
+          v-if="!$slots.pagination"
+          @size-change="handleSizeChange"
+          @current-change="handlePageCurrentChange"
+          :current-page="page.currentPage"
+          :page-sizes="[1, 2, 5, 10]"
+          :page-size="page.pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total"
+        >
+        </el-pagination>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { query } from '@/api/common'
 import { Message } from 'element-ui'
-
+const position = ['left', 'center', 'right']
 export default {
   name: 'DTable',
   components: {},
@@ -86,6 +153,11 @@ export default {
     isRadio: {
       type: Boolean,
       default: true
+    },
+    pagePosition: {
+      type: String,
+      validator: (value) => position.includes(value),
+      default: 'center'
     }
   },
   computed: {
@@ -113,7 +185,16 @@ export default {
   },
   data() {
     return {
-      queryTableData: []
+      // 默认查询方法返回的数据
+      queryTableData: [],
+      // 当前行数据
+      currenRow: null,
+      // 总共多少条数据
+      total: 0,
+      page: {
+        currentPage: 1,
+        pageSize: 10
+      }
     }
   },
   mounted() {
@@ -125,17 +206,49 @@ export default {
       query(this.dataSource).then((res) => {
         if (res?.code === 200) {
           this.queryTableData = res.data.rows
-          console.log(this.queryTableData)
+          this.total = res.data.count
         } else {
           Message.error(res.message)
         }
       })
     },
-    handleCurrentChange(currentRow, oldCurrentRow, index) {
-      console.log(currentRow, oldCurrentRow, index)
+    // 点击表格行事件
+    handleTableCurrentChange(currentRow) {
+      this.currenRow = currentRow
+    },
+    handleSizeChange(val) {
+      this.page.pageSize = val
+    },
+    handlePageCurrentChange(val) {
+      this.page.currentPage = val
     }
   }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.d-table-container {
+  .d-table-options-container {
+    .el-form {
+      display: flex;
+      .el-form-item {
+        margin-right: 10px;
+        margin-bottom: 10px;
+      }
+    }
+  }
+  .d-table-pagination-container {
+    margin: 5px 0;
+  }
+  .d-table-pagination-center {
+    display: flex;
+    justify-content: center;
+  }
+  .d-table-pagination-left {
+    float: left;
+  }
+  .d-table-pagination-right {
+    float: right;
+  }
+}
+</style>
--- src/views/system/DMenuManager.vue
+++ src/views/system/DMenuManager.vue
@@ -27,10 +27,15 @@
           value: 'component'
         }
       ]"
+      page-position="left"
     >
-      <!--<el-table-column prop="date" label="日期" width="180"> </el-table-column>-->
-      <!--<el-table-column prop="name" label="姓名" width="180"> </el-table-column>-->
-      <!--<el-table-column prop="address" label="地址"> </el-table-column>-->
+      <!--<template slot="table">-->
+      <!--  <el-table-column prop="date" label="日期" width="180">-->
+      <!--  </el-table-column>-->
+      <!--  <el-table-column prop="name" label="姓名" width="180">-->
+      <!--  </el-table-column>-->
+      <!--  <el-table-column prop="address" label="地址"> </el-table-column>-->
+      <!--</template>-->
     </d-table>
     <!--新增菜单弹窗-->
     <d-dialog
